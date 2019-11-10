5e06f0dbf84e9a0608d006b7a1cdf9ac43295b38
Christopher <ua.anton.sams@gmail.com>
2019-11-10 13:35:45 +0200

    Formated as PEP8

--- Calculator.py
+++ Calculator.py
@@ -9,16 +9,20 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 
 from kivy.config import Config
-Config.set('graphics',"resizable", 1)
+
+Config.set('graphics', "resizable", 1)
 Config.set('graphics', 'width', 300)
 Config.set('graphics', 'height', 500)
 
+
 class Calculator(App):
     Window.clearcolor = (.9, .9, .9, .9)
+
     def clear_one(self, instance):
         if len(self.formula):
             self.formula[:-1]
         self.update_lable()
+
     def clear(self, istance):
         self.formula = "0"
         self.update_lable()
@@ -27,15 +31,18 @@ class Calculator(App):
         if (self.formula == "0"):
             self.formula = ""
         sz = len(self.formula)
-        if instance.text=="." and (self.formula[sz-1]=="." or self.formula[sz-1] == "" or self.formula[sz-1] == "+" or self.formula[sz-1] == "-" or self.formula[sz-1] == "*" or self.formula[sz-1] == "/"):
-            self.formula +=""
+        if instance.text == "." and (
+                self.formula[sz - 1] == "." or self.formula[sz - 1] == "" or self.formula[sz - 1] == "+" or
+                self.formula[sz - 1] == "-" or self.formula[sz - 1] == "*" or self.formula[sz - 1] == "/"):
+            self.formula += ""
         else:
-            self.formula += str(instance.text) 
+            self.formula += str(instance.text)
         self.update_lable()
 
     def add_operator(self, instance):
         sz = len(self.formula) - 1
-        if self.formula[sz] == "" or self.formula[sz] == "+" or self.formula[sz] == "-" or self.formula[sz] == "*" or self.formula[sz] == "/":
+        if self.formula[sz] == "" or self.formula[sz] == "+" or self.formula[sz] == "-" or self.formula[sz] == "*" or \
+                self.formula[sz] == "/":
             self.formula += ""
         else:
             self.formula += str(instance.text)
@@ -51,39 +58,45 @@ class Calculator(App):
     def build(self):
         root = FloatLayout()
         self.formula = "0"
-        bl = BoxLayout(orientation='vertical', padding = 0)
-        gl = GridLayout(cols = 4, spacing = 3, size_hint = (1, .6))
+        bl = BoxLayout(orientation='vertical', padding=0)
+        gl = GridLayout(cols=4, spacing=3, size_hint=(1, .6))
 
         ##rewrite pls, Igor, u can!
-        self.lbl = Label(text = "0",color = (0,0,0,1), font_size = 45, halign="right", size_hint = (1.5, .4), text_size = (400 - 50, 500 * .4 - 50))
+        self.lbl = Label(text="0", color=(0, 0, 0, 1), font_size=45, halign="right", size_hint=(1.5, .4),
+                         text_size=(400 - 50, 500 * .4 - 50))
         bl.add_widget(self.lbl)
 
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="7", on_press=self.add_number))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="8", on_press=self.add_number))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="9", on_press=self.add_number))
+        gl.add_widget(
+            Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="*", on_press=self.add_operator))
 
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "7", on_press = self.add_number))
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text="8", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "9", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "*", on_press = self.add_operator) )
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="4", on_press=self.add_number))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="5", on_press=self.add_number))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="6", on_press=self.add_number))
+        gl.add_widget(
+            Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="/", on_press=self.add_operator))
 
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "4",on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "5", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "6", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "/", on_press = self.add_operator))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="1", on_press=self.add_number))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="2", on_press=self.add_number))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="3", on_press=self.add_number))
+        gl.add_widget(
+            Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="+", on_press=self.add_operator))
 
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "1", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "2", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "3", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "+",on_press = self.add_operator) )
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text=".", on_press=self.add_number))
+        gl.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="0", on_press=self.add_number))
+        gl.add_widget(
+            Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="=", on_press=self.culc_result))
+        gl.add_widget(
+            Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="-", on_press=self.add_operator))
 
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = ".", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "0", on_press = self.add_number) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "=", on_press = self.culc_result) )
-        gl.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "-", on_press = self.add_operator) )
+        bl1 = BoxLayout(orientation='vertical', padding=(0, 3), size_hint=(1, .1))
+        gl1 = GridLayout(cols=2, spacing=3)
 
-        bl1 = BoxLayout(orientation='vertical', padding = (0, 3), size_hint = (1, .1))
-        gl1 = GridLayout(cols = 2, spacing = 3)
-
-        gl1.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "C", on_press = self.clear))
-        gl1.add_widget( Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3),text = "CE", on_press = self.clear_one) )
+        gl1.add_widget(Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="C", on_press=self.clear))
+        gl1.add_widget(
+            Button(color=(0, 0, 0, 1), background_color=(.9, .9, .9, .3), text="CE", on_press=self.clear_one))
 
         bl1.add_widget(gl1)
 
@@ -92,5 +105,7 @@ class Calculator(App):
         root.add_widget(bl)
 
         return root
+
+
 if __name__ == "__main__":
     Calculator().run()
